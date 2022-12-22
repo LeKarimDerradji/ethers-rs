74d1256be2355b4b3dc1114ab1e2ceaf0c7946b5
LeKarimDerradji <karim.derradji.pro@gmail.com>
2022-12-22 18:43:48 +0100

    modules_2

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use ethers::{
     providers::{Http, Provider},
 };
 
-use dotenv::dotenv;
 
 use crate::provider::providermod;
 
@@ -12,21 +11,6 @@ mod provider;
 
 #[tokio::main]
 async fn main() {
-    // Load environment variables from the .env file
-    dotenv().ok();
-
-    // Get the value of the "INFURA_API_KEY" environment variable
-    let infura_api_key = std::env::var("INFURA_API_KEY").unwrap_or_else(|_| "default value".to_string());
-    println!("INFURA_API_KEY = {}", infura_api_key);
-
-    // Concatenate the API key to the API string
-    let api_string = format!("https://mainnet.infura.io/v3/{}", infura_api_key);
-
-    /*
-    Create a provider for connecting to the Ethereum network
-    */
-    let provider = Provider::<Http>::try_from(api_string.as_str()).unwrap();
-
     // Get the balance of an Ethereum address
     let address_slice =
         b"\xd8\xdA\x6B\xF2\x69\x64\xaf\x9D\x7e\xEd\x9e\x03\xE5\x34\x15\xD3\x7a\xA9\x60\x45";
@@ -39,7 +23,7 @@ async fn main() {
         Err(error) => println!("Error: {}", error),
     }
 
-    let block_number_future = provider.get_block_number();
-    let block_number = block_number_future.await.unwrap();
-    println!("Current block number: {}", block_number);
+    let block_number_future = providermod::get_block_number();
+    //let block_number = block_number_future.await;
+    //println!("Current block number: {}", block_number);
 }
